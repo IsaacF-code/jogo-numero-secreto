1d923eb1ba0f1b2886a474b9e0190c8b0e8a5078
Isaac Freires Santos <isaacfreires@gmail.com>
2024-03-02 11:58:28 -0300

    Adicionado as respostas das perguntas que faltavam

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Questão 1
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -11,13 +12,45 @@ function exibirNoConsole() {
 
 exibirNoConsole();
 
+// Questão 2
 function pegaInput() {
     let valorInput = document.querySelector('input').value;
     console.log(`Olá, ${valorInput}!`);
 }
 
+// Questão 3
 function enviarNumero() {
-    let valorNumero = document.querySelector('input').value;
+    let valorNumero = document.getElementById('valorNumero').value
     let dobraValor = valorNumero * 2;
     console.log(`O valor dobrado de ${valorNumero} é = ${dobraValor}`);
+}
+
+// Questão 4
+function mediaValor() {
+    let numero1 = parseInt(prompt("Digite qualquer valor:"));
+    let numero2 = parseInt(prompt("Digite outro valor:"));
+    let numero3 = parseInt(prompt("Digite mais um valor:"));
+
+    let valorMedia = (numero1 + numero2 + numero3)/3
+    console.log(`A média dos números: ${numero1}, ${numero2} e ${numero3}, é = ${valorMedia}`);
+}
+
+// Questão 5
+function maiorNumero() {
+    let numero1 = parseInt(prompt("Digite um número:"));
+    let numero2 = parseInt(prompt("Digite outro número:"));
+
+    if(numero1 > numero2) {
+        console.log(`O maior número é ${numero1}`);
+    } else {
+        console.log(`O maior número é ${numero2}`);
+    }
+}
+
+// Questão 6
+function valorMultiplicado() {
+    let numero = document.getElementById('valorMultiplicado').value;
+    let valorMultiplicadoPorEleMesmo = numero * numero;
+
+    console.log(`O resultado é ${valorMultiplicadoPorEleMesmo}`);
 }
