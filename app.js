29187571cedc9c145034e835824a97a049d5b5b8
Isaac Freires Santos <isaacfreires@gmail.com>
2024-03-05 11:07:06 -0300

    Finalizado questões

---
+++ app.js
@@ -0,0 +1,20 @@
+// Questão 1
+let listaGenerica = [];
+
+// Questão 2
+let linguagensDeProgramacao = ['Javascript', 'C', 'C++', 'Kotlin', 'Python'];
+console.log("Lista: ", linguagensDeProgramacao);
+
+// Questão 3
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log("Lista atualizada: ", linguagensDeProgramacao);
+
+// Questão 4
+let jogos = ['Uncharted', 'The Last Of Us', 'Batman Arkham'];
+console.log("Lista jogos primeiro elemento: ", jogos[0]);
+
+// Questão 5
+console.log("Lista jogos segundo elemento: ", jogos[1]);
+
+// Questão 6
+console.log("Lista jogos último elemento: ", jogos[2]);
